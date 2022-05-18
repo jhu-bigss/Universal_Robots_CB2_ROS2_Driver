77cf3d8a73a350f2887862c81eb8cd7f357d2010
Joshua Liu <liushuya7@gmail.com>
2022-05-18 01:06:05 -0400

    change launch_rviz default to false

--- ur_bringup/launch/ur_control.launch.py
+++ ur_bringup/launch/ur_control.launch.py
@@ -333,7 +333,7 @@ def generate_launch_description():
         )
     )
     declared_arguments.append(
-        DeclareLaunchArgument("launch_rviz", default_value="true", description="Launch RViz?")
+        DeclareLaunchArgument("launch_rviz", default_value="false", description="Launch RViz?")
     )
 
     return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
