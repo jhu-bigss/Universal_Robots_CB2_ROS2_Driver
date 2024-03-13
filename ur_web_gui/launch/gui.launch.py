941553444e6d17b19d49f1ec247ecb761bb82fe5
Joshua Liu <jsliu@jhu.edu>
2024-03-13 17:59:32 -0400

    add launch file

---
+++ ur_web_gui/launch/gui.launch.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='ur_web_gui',
+            executable='ur_web_gui',
+            parameters=[{
+                'ur_type': 'ur10e',
+            }],
+        ),
+    ])
--- ur_web_gui/setup.py
+++ ur_web_gui/setup.py
@@ -1,3 +1,5 @@
+from glob import glob
+
 import xml.etree.ElementTree as ET
 
 from setuptools import find_packages, setup
@@ -13,6 +15,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
